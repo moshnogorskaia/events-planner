0bd2d4ac4bf298340304bfbc623ec04915c943a3
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-12-10 13:27:47 +0100

    Add a page with dynamic route

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,32 @@
+import { getEventById } from '../../dummy-data';
+import { useRouter } from 'next/router';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+
 function EventDetailPage() {
-    return <div>EventDetail</div>;
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <div>Event not found</div>;
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 }
 
 export default EventDetailPage;
