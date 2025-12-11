60673e1b60e91585527d63131c16432ab45b088c
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-12-11 08:29:27 +0100

    Extract filter data from the dynamic path

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,5 +1,41 @@
+import { useRouter } from 'next/router';
+import { getFilteredEvents } from '../../dummy-data';
+
 function FilteredEventsPage() {
-    return <div>FilteredEvents</div>;
+  const router = useRouter();
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p className="center">Invalid filter. Please adjust your values.</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p className="center">No events found for the chosen filter.</p>;
+  }
+
+  return <EventList events={filteredEvents} />;
 }
 
 export default FilteredEventsPage;
