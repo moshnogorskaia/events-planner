fb0b630fb9d93eaa8fd961eb91b6c6e02379e8a5
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-12-22 09:37:38 +0100

    Optimize data fetching

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,4 @@
-import { getEventById, getAllEvents } from '../../helpers/api-util';
+import { getEventById, getFeaturedEvents } from '../../helpers/api-util';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
@@ -7,7 +7,11 @@ function EventDetailPage(props) {
   const event = props.event;
 
   if (!event) {
-    return <div>Event not found</div>;
+    return (
+      <div className="center">
+        <p>Loading...</p>
+      </div>
+    );
   }
 
   const { title, date, location, image, imageAlt, description } = event;
@@ -34,18 +38,19 @@ export async function getStaticProps(context) {
   return {
     props: {
       event,
+      revalidate: 30,
     },
   };
 }
 
 export async function getStaticPaths() {
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
   const paths = events.map((event) => ({
     params: { eventId: event.id },
   }));
   return {
     paths,
-    fallback: false,
+    fallback: 'blocking',
   };
 }
 
