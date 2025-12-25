27b67728b6fd4e3c620d81ea09ecb037ae0e19f9
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-12-25 20:04:43 +0100

    Optimize images

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -3,6 +3,7 @@ import AddressIcon from '../icons/address-icon';
 import ArrowRightIcon from '../icons/arrow-right-icon';
 import classes from './event-item.module.css';
 import Button from '../ui/button';
+import Image from 'next/image';
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -18,7 +19,7 @@ function EventItem(props) {
 
   return (
     <li className={classes.item}>
-      <img src={`/${image}`} alt={title} />
+      <Image src={`/${image}`} alt={title} width={300} height={300} />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
